bb8857c4142e5dbfe2c87baf99df4a48a0504f34
Petr Pivoňka <petrpiv1@gmail.com>
2025-12-17 02:30:36 +0100

    Fix namespaces

--- Src/BigBang1112.EssentialsV2/App.cs
+++ Src/BigBang1112.EssentialsV2/App.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 using System.Reflection;
 
 namespace BigBang1112;
